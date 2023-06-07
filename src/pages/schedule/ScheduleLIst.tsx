8d00c37f738a69275b2af87f3193745971f31bf9
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-06-07 14:06:31 +0500

    ADD DOSE SCHEDULE

--- src/pages/schedule/ScheduleLIst.tsx
+++ src/pages/schedule/ScheduleLIst.tsx
@@ -1,16 +1,19 @@
 import {
   IonCard,
   IonContent,
-  IonHeader,
+  IonDatetime,
+  IonIcon,
   IonItem,
+  IonLabel,
   IonPage,
-  IonTitle,
-  IonToolbar,
+  IonPopover,
 } from "@ionic/react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import HeaderButtons from "../../components/HeaderButtons";
 import Schedulecard from "../../components/Schedule-card/Schedulecard";
 import { groupBy } from "lodash";
+import { calendar } from "ionicons/icons";
+import { format } from "date-fns";
 
 interface IDoseSchedule {
   Id: number;
@@ -18,7 +21,7 @@ interface IDoseSchedule {
   MinAge: number;
   MinGap: number;
   VaccineId: number;
-  
+  DoseDate: string;
 }
 
 interface TGroupData {
@@ -29,105 +32,79 @@ interface TGroupData {
 const ScheduleList: React.FC = () => {
   const [data, setData] = useState<IDoseSchedule[]>([]);
   const [groupedData, setGroupData] = useState<TGroupData[]>([]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [error, setError] = useState(false);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const inputRef = useRef(null);
+  const [inputValue, setInputValue] = useState('');
+
+  // const [error, setError] = useState(false);
+  const [showPopover, setShowPopover] = useState(false);
+  // const [selectedDate, setSelectedDate] = useState<string>("");
+  const handleIconClick = () => {
+    setShowDatePicker(true);
+  };
+ 
 
   useEffect(() => {
     fetchVaccineData();
   }, [location]);
-
   const fetchVaccineData = async () => {
     // setShowLoading(true);/
-    fetch(`https://myapi.fernflowers.com/doseschedule_date`)
+    fetch(`http://localhost:5041/doseschedule_date`)
       .then((response) => response.json())
       .then((data) => {
         setData(data);
-
-        // const updatedData = data.map(data=> {
-        //     const dateString = data.DoseDate;
-        //     const dateObject = new Date(dateString);
-        //     return {
-        //       ...data,
-        //       date: dateObject
-        //     };
-        //   });
-
-        //   // Log the updated data with Date objects
-        //   console.log(updatedData);
-        // for(var key in data) {
-
-        //     const date= data.DoseDate.dateTimeString.split(/[T ]/);
-        //    console.log(date)
-        //     }
-        //       // Example usage
-        //       const apiData = [
-        //         { id: 1, date: "2023-06-05T06:33:44.8808365-05:00", name: "John" },
-        //         { id: 2, date: "2023-06-05T09:15:30.1234567-05:00", name: "Jane" },
-        //         { id: 3, date: "2023-06-06T14:30:00.9876543-05:00", name: "Bob" },
-        //         { id: 4, date: "2023-06-06T16:45:20.5678901-05:00", name: "Alice" },
-        //       ];
-
-        //   const groupedData = groupDataByDate(data);
-        //   console.log(groupedData);
-
-        console.log(data); // Display the retrieved data in the console
-
-        // const groupedData = data.reduce((groups, item) => {
-        //     // data.DoseDate.dateTimeString.split(/[T ]/);
-
-        //     const { DoseDate, Name } = item;
-        //     if (!groups[DoseDate]) {
-        //       groups[DoseDate] = [];
-        //     }
-        //     groups[DoseDate].push(Name);
-        //     return groups;
-        //   }, {});
-        //   setData(data)
-        //   console.log(groupedData);
-        // const dataArray = data.map(item =>{
-        //     return {
-        //       element1: item.DoseDate, // Replace 'property1' with the actual property you want to include as element 1
-        //       element2: item.Id,
-        //       element3: item.Name, // Replace 'property2' with the actual property you want to include as element 2
-        //     };
-        //   }); // Replace 'property' with the actual property you want to include in the array
-        // console.log(dataArray);
-        // function groupItems(array , property) {
-        //     return array.reduce(function(groups, item) {
-        //         var name = item[property]
-        //         var group = groups[name] || (groups[name] = []);
-        //         group.push(item);
-        //         return groups;
-        //     }, { });
-        // }
-        // var groups = groupItems(Array, 'date');
-        // for(var key in groups) {
-        //     var group = groups[key];
-        //     console.log('"' + key + '"\t' + JSON.stringify(group, null, 4))
-        // }
       });
-
-    //   .then((data: IVaccineData[]) => {
-    //     setData(data);
-    //     setShowLoading(false);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //     setShowLoading(false);
-    //   })
-    //   .finally(() => {
-    //     setShowLoading(false);
-    //   });
   };
-
-  useEffect(() => {
+  const handleDateChange = (event: CustomEvent,key: string ) => {
+    console.log(setInputValue(event.detail.value));
+    console.log(selectedDate);
+    setSelectedDate(event.detail.value);
+    closePopover();
+    // const previousDate = objectItem.key; // Get the previous date from the objectItem
+    // console.log('Previous Date:', previousDate);
+    const data=event.detail.value
+    const data1=data.split('T');
+    const data2=data1[0];
+    console.log(data2);
    
-
+    console.log(event.detail.value);
+    const data3='2028-06-05';
+    const dataTobeSent = [
+      {
+      path:"Date",
+      op:"replace",
+      from:data3,
+      value:data2,
+    }
+  ];
+    console.log("object item date : ", dataTobeSent );
+    fetch(`http://localhost:5041/api/AdminDoseSchedule/Admin_bulk_updateDate/${data3}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataTobeSent),
+      }
+    )
+      .then((response) => {
+        // if (response.status === 204) renderList();
+      })
+      .catch((err) => {
+        setError(true);
+      });
+    // UpdateExpiryDateOfDoctor(event.detail.value);
+  };
+  useEffect(() => {
+    const savedValue = localStorage.getItem('inputValue');
     const groupedData = groupBy(data, (item) => {
       const date = new Date(item.DoseDate);
       return date.toISOString().split("T")[0];
     });
-
     console.log("groupedData", groupedData);
-
     const groupArray = [];
     for (const key in groupedData) {
       if (Object.prototype.hasOwnProperty.call(groupedData, key)) {
@@ -135,45 +112,90 @@ const ScheduleList: React.FC = () => {
         groupArray.push({ key, value: element });
       }
     }
-
     console.log("groupArray", [...groupArray]);
     setGroupData(groupArray);
-
-   
   }, [data]);
+  const openPopover = () => {
+    setShowPopover(true);
+  };
 
+  const closePopover = () => {
+    setShowPopover(false);
+  };
   return (
     <IonPage>
-      <HeaderButtons pageName="" Showbutton={true} url="" />
+       <HeaderButtons
+          pageName="Dose Schedule"
+          Showbutton={true}
+          url="/members/schedule"
+        />
       <IonContent className="ion-padding">
-        
-      {data &&
-      groupedData.map((objectItem: any, index: number) => {
-        const array = objectItem.value;
-        return (
-          <React.Fragment key={index}>
-            <IonCard>
-            <IonItem lines="none" className="ion-text-center ion-text-sm-start">
-              <h2 >{objectItem.key}</h2></IonItem>
-            {array.map((item: any, itemIndex: number) => (
-            <IonItem key={itemIndex}>
-               <Schedulecard
-                Id={item.Id}
-                Name={item.Name}
-                MinAge={item.MinAge}
-                MinGap={item.MinGap}
-                VaccineId={item.VaccineId}
-              />
-            </IonItem>
-            ))}
-            </IonCard>
-          </React.Fragment>
-        );
-      })}
-      
-      </IonContent>
-    </IonPage>
+        {data &&
+          groupedData.map((objectItem: any, index: number) => {
+            const ourdate = objectItem.value.DoseDate;
+            // console.log(ourdate)
+            const array = objectItem.value;
+            return (
+              <React.Fragment key={index}>
+                <IonCard>
+                  <IonItem lines="none" className="centered-item">
+                    <IonLabel style={{ textAlign: "center" }}>
+                      <IonItem
+                        lines="none"
+                        slot="center"
+                        style={{ textAlign: "center", padding: 0 }}
+                      >
+                        <IonIcon
+                        color="primary"
+                          onClick={() => setShowPopover(true)}
+                          icon={calendar}
+                        />
+                        <input
+                          style={{ border: "none" }}
+                          type="date"
+                          readOnly
+                          value={objectItem.key}
+                        />
+                        <IonPopover
+                          isOpen={showPopover}
+                          onDidDismiss={closePopover}
+                        >
+                          <IonDatetime
+                            // displayFormat="MMM DD, YYYY"
+                            placeholder="Select Date"
+                            value={inputValue|| undefined}
+                            onIonChange={(e) => handleDateChange(e, objectItem.key)}
+                          ></IonDatetime>
+                        </IonPopover>
+                      </IonItem>
+                    </IonLabel>
+                  </IonItem>
+                  {array.map((item: any, itemIndex: number) => (
+                    <IonItem key={itemIndex}>
+                      <Schedulecard
+                        Id={item.Id}
+                        Name={item.Name}
+                        MinAge={item.MinAge}
+                        MinGap={item.MinGap}
+                        VaccineId={item.VaccineId}
+                        DoseDate={item.DoseDate}
+                        cardDate={ourdate}
+                      />
+                    </IonItem>
+                  ))}
+                  </IonCard>
+             </React.Fragment>
+              
+            );
+          })}
+          
+         </IonContent>
+         </IonPage>
+
   );
 };
-
 export default ScheduleList;
+function fetchOtherSourceData() {
+  throw new Error("Function not implemented.");
+}
+
