0439cc51def7ba8ada6bf7989e728892824c4289
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-06-08 14:36:25 +0500

    apk form

--- src/components/Schedule-card/Schedulecard.tsx
+++ src/components/Schedule-card/Schedulecard.tsx
@@ -3,61 +3,75 @@ import {
   IonDatetime,
   IonIcon,
   IonItem,
-  IonPopover
+  IonPopover,
+  IonGrid,
+  IonRow,
+  IonCol
 } from "@ionic/react";
 import { format } from "date-fns";
 import { calendar } from "ionicons/icons";
 
 import AlertError from "../alerts/AlertError";
 import AlertSuccess from "../alerts/AlertSuccess";
+
 interface IDoseSchedule {
   Id: number;
   Name: string;
   MinAge: number;
   MinGap: number;
   VaccineId: number;
-  DoseDate: string
+  DoseDate: string;
   cardDate: string;
   renderList: () => void;
 }
-const Schedulecard: React.FC<IDoseSchedule> = ({ Name, DoseDate ,Id, cardDate, renderList}) => {
+
+const Schedulecard: React.FC<IDoseSchedule> = ({
+  Name,
+  DoseDate,
+  Id,
+  cardDate,
+  renderList
+}) => {
   const [error, setError] = useState(false);
   const [showPopover, setShowPopover] = useState(false);
   const [selectedDate, setSelectedDate] = useState<string>("");
   const [success, setSuccess] = useState(false);
- 
-  const handleDateChange =async (event: CustomEvent<any>) => {
+
+  const handleDateChange = async (event: CustomEvent<any>) => {
     const selectedValue = event.detail.value;
     console.log(selectedValue);
-    const data=selectedValue.split('T');
-    const data1=data[0];
+    const data = selectedValue.split("T");
+    const data1 = data[0];
     console.log(data1);
-      const dataTobeSent = {
-    date: selectedValue,
-    doseId: Id
-      };
-      console.log(dataTobeSent)
-      try {
-        const response = await fetch(`https://myapi.fernflowers.com/api/AdminDoseSchedule/Admin_single_updateDate`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(dataTobeSent),
-      })
+    const dataTobeSent = {
+      date: selectedValue,
+      doseId: Id
+    };
+    console.log(dataTobeSent);
+    try {
+      const response = await fetch(
+        `https://myapi.fernflowers.com/api/AdminDoseSchedule/Admin_single_updateDate`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(dataTobeSent)
+        }
+      );
       if (response.ok) {
-        console.log(response.ok)
+        console.log(response.ok);
         setSuccess(true);
         renderList();
         setShowPopover(false);
       } else if (!response.ok) {
         setError(true);
       }
-      }catch (error) {
-        console.error(error);
-        setError(true);
-        setShowPopover(false);
-      }
+    } catch (error) {
+      console.error(error);
+      setError(true);
+      setShowPopover(false);
+    }
   };
 
   const openPopover = () => {
@@ -70,7 +84,7 @@ const Schedulecard: React.FC<IDoseSchedule> = ({ Name, DoseDate ,Id, cardDate, r
 
   return (
     <>
-    <AlertSuccess
+      <AlertSuccess
         isOpen={success}
         setOpen={setSuccess}
         message="Selected dose date updated successfully"
@@ -78,31 +92,41 @@ const Schedulecard: React.FC<IDoseSchedule> = ({ Name, DoseDate ,Id, cardDate, r
       <AlertError
         isOpen={error}
         setOpen={setError}
-        message="An Error occcured. Plz try again."
+        message="An Error occurred. Please try again."
       />
-        <IonItem>
-        {Name}
-        </IonItem>
-      <IonItem slot="end" lines="none" style={{ padding: 0 }}>
-  <IonItem>
-    <IonIcon color="primary" onClick={() => setShowPopover(true)} icon={calendar} />
-    <input
-      style={{ border: "none" }}
-      type="date"
-      readOnly
-      value={DoseDate ? format(new Date(DoseDate), "yyyy-MM-dd") : ""}
-    />
-  </IonItem>
-  <IonPopover isOpen={showPopover} onDidDismiss={closePopover}>
-    <IonDatetime
-      placeholder="Select Date"
-      value={selectedDate || undefined}
-      onIonChange={handleDateChange}
-    ></IonDatetime>
-  </IonPopover>
-</IonItem>
-
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <>
+              {Name}
+            </>
+          </IonCol>
+          <IonCol size="auto">
+            < >
+              <IonIcon
+                color="primary"
+                onClick={() => setShowPopover(true)}
+                icon={calendar}
+              />
+              <input
+                style={{ border: "none" }}
+                type="date"
+                readOnly
+                value={DoseDate ? format(new Date(DoseDate), "yyyy-MM-dd") : ""}
+              />
+            </>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      <IonPopover isOpen={showPopover} onDidDismiss={closePopover}>
+        <IonDatetime
+          placeholder="Select Date"
+          value={DoseDate|| undefined}
+          onIonChange={handleDateChange}
+        ></IonDatetime>
+      </IonPopover>
     </>
   );
 };
+
 export default Schedulecard;
