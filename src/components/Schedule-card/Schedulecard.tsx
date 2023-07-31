94962c68ab687825bfbfe9d1a76770e34c3bc0c9
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-07-31 19:07:41 +0500

    admin dose schedule fixed and implemented

--- src/components/Schedule-card/Schedulecard.tsx
+++ src/components/Schedule-card/Schedulecard.tsx
@@ -7,6 +7,10 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonCard,
+  IonItem,
+  IonLabel,
+  IonText,
 } from "@ionic/react";
 import { calendar } from "ionicons/icons";
 
@@ -16,29 +20,162 @@ import { format } from "date-fns";
 import Toast from "../Custom Toast/Toast";
 
 interface IDoseSchedule {
-  Id: number;
-  date: string;
-  Name: string;
-  MinAge: number;
-  VaccineId: number;
-  renderList: () => void;
+  key:string,
+  date: any,
+  data:any,
+  renderList: () => void
 }
 
 const Schedulecard: React.FC<IDoseSchedule> = ({
-  Name,
-  Id,
+  // Name,
+  // Id,
+  key,
   date,
+  data,
   renderList,
 }) => {
   const [error, setError] = useState(false);
   const [showPopover, setShowPopover] = useState(false);
   const [selectedDate, setSelectedDate] = useState<string | undefined>(date); // Initialize with the `date` prop value
-
+  const [showPopover2, setShowPopover2] = useState(false);
   const [success, setSuccess] = useState(false);
-  const handelonmouseover = (inputValue: string) => {
+  const [value, setValue] = useState("");
+  const [singleId,setSingleId] = useState('');
+
+  const handelonmouseover2 = (inputValue: string) => {
+        // const data1 = inputValue.split("T");
+        const data2 = format(new Date(inputValue), "yyyy-MM-dd");
+        setValue(data2);
+        setSelectedDate(data2);
+      };
+    
+      const handleDateChange2 = async (
+        event: CustomEvent,
+        key: string,
+        inputValue: string
+      ) => {
+        console.log(value);
+        closePopover();
+        const data = event.detail.value;
+        // const data1 = data.split("T");
+        // const data2 = data1[0];
+        // console.log(data2);
+    console.log(data)
+        console.log(event.detail.value);
+    
+        // const dataTobeSent = [
+        //   {
+        //     path: "Date",
+        //     op: "replace",
+        //     from: value,
+        //     value: data2,
+        //   },
+        // ];
+    
+        // console.log("object item date : ", dataTobeSent);
+        try {
+          // setShowLoading(true);
+          const response = await fetch(
+            `${
+              import.meta.env.VITE_API_URL
+            }api/AdminSchedule/admin_bulk_update_Date?oldDate=${value}&newDate=${data}`,
+            {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              // body: JSON.stringify(dataTobeSent),
+            }
+          );
+          console.log(
+            `${
+              import.meta.env.VITE_API_URL
+            }api/AdminSchedule/admin_bulk_update_Date?oldDate=${value}&newDate=${data}`
+          );
+          if (response.status === 204) {
+            // setShowLoading(false);
+            console.log(response.ok);
+            setSuccess(true);
+            renderList();
+          } else if (!response.ok) {
+            setError(true);
+            // setShowLoading(false);
+          }
+        } catch (error) {
+          console.error(error);
+          setError(true);
+          // setShowLoading(false);
+        }
+      };
+
+
+  // const handelonmouseover2 = (inputValue: string) => {
+  //   // const data1 = inputValue.split("T");
+  //   const data2 = format(new Date(inputValue), "yyyy-MM-dd");
+  //   console.log(data2)
+  //   setValue(data2);
+  //   setSelectedDate(data2);
+  // };
+
+  // const handleDateChange2 = async (
+  //   event: CustomEvent,
+  //   key: string,
+  //   value: string
+  // ) => {
+  //   console.log(value);
+  //   closePopover();
+  //   const data = event.detail.value;
+  //   // const data1 = data.split("T");
+  //   // const data2 = data1[0];
+  //   // console.log(data2);
+
+  //   console.log(event.detail.value);
+
+  //   // const dataTobeSent = [
+  //   //   {
+  //   //     path: "Date",
+  //   //     op: "replace",
+  //   //     from: value,
+  //   //     value: data,
+  //   //   },
+  //   // ];
+
+  //   // console.log("object item date : ", dataTobeSent);
+  //   try {
+  //     // setShowLoading(true);
+  //     const response = await fetch(
+  //       `${
+  //         import.meta.env.VITE_API_URL
+  //       }api/DoctorSchedule/doctor_bulk_update_Date?DoctorId=${storedValue.Id}&oldDate=${value}&newDate=${data}`,
+  //       {
+  //         method: "PATCH",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         // body: JSON.stringify(dataTobeSent),
+  //       }
+  //     );
+  //     if (response.status === 204) {
+  //       console.log(response.ok);
+  //       renderList();
+  //       setSuccess(true);
+  //       // setShowLoading(false);
+  //     } else if (!response.ok) {
+  //       setError(true);
+  //       // setShowLoading(false);
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     setError(true);
+  //     // setShowLoading(false);
+  //   }
+  // };
+
+
+  const handelonmouseover = (inputValue: string,Id: string) => {
     // const data1 = inputValue.split("T");
     const data2 = format(new Date(inputValue), "yyyy-MM-dd");
-
+    setSingleId(Id);
     setSelectedDate(data2);
   };
   const handleDateChange = async (event: CustomEvent<any>) => {
@@ -46,7 +183,7 @@ const Schedulecard: React.FC<IDoseSchedule> = ({
     console.log(selectedValue);
     const dataTobeSent = {
       date: selectedValue,
-      doseId: Id,
+      doseId: singleId,
     };
     console.log(dataTobeSent);
     try {
@@ -81,34 +218,61 @@ const Schedulecard: React.FC<IDoseSchedule> = ({
     setShowPopover(false);
   };
 
+  const closePopover2 = () => {
+    setShowPopover2(false);
+  };
+
   return (
     <>
      <Toast
-          isOpen={success}
-          setOpen={setSuccess}
-          message="Single date of admin schedule update successfully."
-          color="success"
-        />
-        <Toast
-          isOpen={error}
-          setOpen={setError}
-          message="An error occurred while update single date of admin schedule. plz try again"
-          color="danger"
-        />
-      {/* <AlertSuccess
         isOpen={success}
         setOpen={setSuccess}
-        message="Selected dose date updated successfully"
+        message="Date updated successfully"
+        color="success"
       />
-      <AlertError
+      <Toast
         isOpen={error}
         setOpen={setError}
-        message="An Error occurred. Please try again."
-      /> */}
+        message="an Error occurred while updating date, please try again later"
+        color="danger"
+      />
+      <IonCard key={key}>
+        <>
+          <IonItem lines="none" className="centered-item">
+            <IonLabel style={{ textAlign: "center" }}>
+              <IonItem
+                lines="none"
+                slot="center"
+                style={{ textAlign: "center", padding: 0 }}
+              >
+                <IonIcon
+                  color="primary"
+                  onClick={() => setShowPopover2(true)}
+                  icon={calendar}
+                  style={{ marginRight: "10px", PointerEvent: "cursor" }}
+                  onMouseOver={() => handelonmouseover2(date)}
+                />
+                {/* <IonText>{format(new Date(date), "yyyy-MM-dd")}</IonText> */}
+                <IonText>{format(new Date(date), "yyyy-MM-dd")}</IonText>
+                <IonPopover isOpen={showPopover2} onDidDismiss={closePopover2}>
+                  <IonDatetime
+                    placeholder="Select Date"
+                    value={selectedDate || undefined}
+                    onIonChange={(e) =>
+                      handleDateChange2(e, date, value)
+                    }
+                  ></IonDatetime>
+                </IonPopover>
+              </IonItem>
+            </IonLabel>
+          </IonItem>
+          {data.map((item: any) =>
+            item !== null ? (
+              <>
       <IonGrid>
         <IonRow>
           <IonCol>
-            <b>{Name}</b>
+            <b>{item.Name}</b>
           </IonCol>
           <IonCol size="auto">
             <>
@@ -117,7 +281,7 @@ const Schedulecard: React.FC<IDoseSchedule> = ({
                 onClick={() => setShowPopover(true)}
                 icon={calendar}
                 style={{ marginRight: "10px", cursor: "pointer" }}
-                onMouseOver={() => handelonmouseover(date)}
+                onMouseOver={() => handelonmouseover(date,item.Id)}
               />
             </>
           </IonCol>
@@ -130,6 +294,13 @@ const Schedulecard: React.FC<IDoseSchedule> = ({
           onIonChange={handleDateChange}
         ></IonDatetime>
       </IonPopover>
+      </>
+            ) : (
+              ""
+            )
+          )}
+        </>
+      </IonCard>
     </>
   );
 };
--- src/pages/schedule/ScheduleList1.tsx
+++ src/pages/schedule/ScheduleList1.tsx
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from "react";
 import {
   IonCard,
-  // IonCol,
+  IonCol,
   IonContent,
   IonDatetime,
-  // IonGrid,
+  IonGrid,
   IonIcon,
   IonItem,
   IonLabel,
   IonPage,
   IonPopover,
-  // IonRow,
+  IonRow,
   IonText,
 } from "@ionic/react";
-// import { groupBy } from "lodash";
-import Schedulecard from "../../components/Schedule-card/Schedulecard";
-import HeaderButtons from "../../components/HeaderButtons";
-import AlertError from "../../components/alerts/AlertError";
-import AlertSuccess from "../../components/alerts/AlertSuccess";
+import { groupBy } from "lodash";
 import { calendar } from "ionicons/icons";
-import LoadingSpinner from "../../components/loading-spinner/LoadingSpinner";
-import { format } from "date-fns";
-import ErrorComponent from "../../components/error-component/ErrorComponent";
-import Toast from "../../components/Custom Toast/Toast";
-// import DatePicker from "../../components/Date Picker/MyDatePicker";
+import HeaderButtons from "../../components/HeaderButtons";
+import Schedulecard from "../../components/Schedule-card/Schedulecard";
+// import { format } from "date-fns";
+// import MyDatePicker from "../../components/datepicker/MyDatePicker";
+// import DoctorScheduleCard from "./DoctorScheduleCard";
+// import Header from "../../components/header/Header";
+// import Toast from "../../components/custom-toast/Toast";
 interface IVaccine {
   Id: number;
   Name: string;
@@ -34,8 +32,10 @@ interface IVaccine {
 interface IVaccineData {
   [date: string]: IVaccine[];
 }
+const storedValue = JSON.parse(sessionStorage.getItem("docData"));
+console.log(storedValue);
 
-const ScheduleList1: React.FC = () => {
+const DoctorScheduleCardList: React.FC = () => {
   const [data, setData] = useState<IVaccine[]>([]);
   const [groupedData, setGroupedData] = useState<IVaccineData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -54,136 +54,36 @@ const ScheduleList1: React.FC = () => {
 
   useEffect(() => {
     fetchDoseData();
+    // window.location.reload();
   }, []);
 
   const fetchDoseData = async () => {
-    try {
-      const response = await fetch(
-        `${
-          import.meta.env.VITE_API_URL
-        }api/AdminSchedule/admin_post_doseSchedule`
-      );
-      if (response.ok) {
-        const data = await response.json();
-        setShowLoading(true);
-        setData(data);
-        console.log(data);
-        setIsLoading(false);
-      } else {
-        console.log("Error fetching data");
-        setIsLoading(false);
-      }
-    } catch (error) {
-      console.log("Error:", error);
-      setIsLoading(false);
-    }
-  };
-
-  const handelonmouseover = (inputValue: string) => {
-    // const data1 = inputValue.split("T");
-    const data2 = format(new Date(inputValue), "yyyy-MM-dd");
-    setValue(data2);
-    setSelectedDate(data2);
-  };
-
-  const handleDateChange = async (
-    event: CustomEvent,
-    key: string,
-    inputValue: string
-  ) => {
-    console.log(value);
-    closePopover();
-    const data = event.detail.value;
-    // const data1 = data.split("T");
-    // const data2 = data1[0];
-    // console.log(data2);
-console.log(data)
-    console.log(event.detail.value);
-
-    // const dataTobeSent = [
-    //   {
-    //     path: "Date",
-    //     op: "replace",
-    //     from: value,
-    //     value: data2,
-    //   },
-    // ];
-
-    // console.log("object item date : ", dataTobeSent);
-    try {
-      setShowLoading(true);
-      const response = await fetch(
-        `${
-          import.meta.env.VITE_API_URL
-        }api/AdminSchedule/admin_bulk_update_Date?oldDate=${value}&newDate=${data}`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          // body: JSON.stringify(dataTobeSent),
+        try {
+          const response = await fetch(
+            `${
+              import.meta.env.VITE_API_URL
+            }api/AdminSchedule/admin_post_doseSchedule`
+          );
+          if (response.ok) {
+            const data = await response.json();
+            setShowLoading(true);
+            setData(data);
+            console.log(data);
+            setIsLoading(false);
+          } else {
+            console.log("Error fetching data");
+            setIsLoading(false);
+          }
+        } catch (error) {
+          console.log("Error:", error);
+          setIsLoading(false);
         }
-      );
-      console.log(
-        `${
-          import.meta.env.VITE_API_URL
-        }api/AdminSchedule/admin_bulk_update_Date?oldDate=${value}&newDate=${data}`
-      );
-      if (response.status === 204) {
-        setShowLoading(false);
-        console.log(response.ok);
-        setSuccess(true);
-        forceRender();
-      } else if (!response.ok) {
-        setError(true);
-        setShowLoading(false);
-      }
-    } catch (error) {
-      console.error(error);
-      setError(true);
-      setShowLoading(false);
-    }
-  };
-
-  const openPopover = () => {
-    setShowPopover(true);
-  };
-
-  const closePopover = () => {
-    setShowPopover(false);
-  };
+      };
 
   return (
     <>
-      <LoadingSpinner
-        isOpen={showLoading}
-        setOpen={setShowLoading}
-        time={3000}
-      />
-      <Toast
-        isOpen={success}
-        setOpen={setSuccess}
-        message="Bulk date of admin schedule update successfully."
-        color="success"
-      />
-      <Toast
-        isOpen={error}
-        setOpen={setError}
-        message="An error occurred while update bulk date of admin schedule. plz try again"
-        color="danger"
-      />
-      {/* <AlertSuccess
-        isOpen={success}
-        setOpen={setSuccess}
-        message="Selected dose date updated successfully"
-      />
-      <AlertError
-        isOpen={error}
-        setOpen={setError}
-        message="An error occurred. Please try again."
-      /> */}
       <IonPage>
-        <HeaderButtons
+      <HeaderButtons
           pageName="Dose Schedule"
           Showbutton={false}
           backbutton={true}
@@ -191,63 +91,12 @@ console.log(data)
         />
         <IonContent className="ion-padding">
           {Object.keys(data).map((date) => (
-            <IonCard key={date}>
-              <>
-                <IonItem lines="none" className="centered-item">
-                  <IonLabel style={{ textAlign: "center" }}>
-                    <IonItem
-                      lines="none"
-                      slot="center"
-                      style={{ textAlign: "center", padding: 0 }}
-                    >
-                      <IonIcon
-                        color="primary"
-                        onClick={() => setShowPopover(true)}
-                        icon={calendar}
-                        style={{ marginRight: "10px", cursor: "pointer" }}
-                        onMouseOver={() => handelonmouseover(date)}
-                      />
-                      <IonText>{format(new Date(date), "yyyy-MM-dd")}</IonText>
-                      <IonPopover
-                        isOpen={showPopover}
-                        onDidDismiss={closePopover}
-                      >
-                        <IonDatetime
-                          placeholder="Select Date"
-                          value={selectedDate || undefined}
-                          onIonChange={(e) =>
-                            handleDateChange(e, date, inputValue)
-                          }
-                        ></IonDatetime>
-                      </IonPopover>
-                    </IonItem>
-                  </IonLabel>
-                </IonItem>
-                {data && data[date] ? (
-                  data[date].length > 0 ? (
-                    data[date].map((item: IVaccine) =>
-                      item !== null ? (
-                        <Schedulecard
-                          key={item.Id}
-                          date={date}
-                          Id={item.Id}
-                          Name={item.Name}
-                          MinAge={item.MinAge}
-                          VaccineId={item.VaccineId}
-                          renderList={forceRender}
-                        />
-                      ) : (
-                        ""
-                      )
-                    )
-                  ) : (
-                    <ErrorComponent title="Admin Schedule" />
-                  )
-                ) : (
-                  <ErrorComponent title="Brands" />
-                )}
-              </>
-            </IonCard>
+            <Schedulecard
+              key={date}
+              date={date}
+              data={data[date]}
+              renderList={forceRender}
+            />
           ))}
         </IonContent>
       </IonPage>
@@ -255,4 +104,271 @@ console.log(data)
   );
 };
 
-export default ScheduleList1;
+export default DoctorScheduleCardList;
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import {
+//   IonCard,
+//   // IonCol,
+//   IonContent,
+//   IonDatetime,
+//   // IonGrid,
+//   IonIcon,
+//   IonItem,
+//   IonLabel,
+//   IonPage,
+//   IonPopover,
+//   // IonRow,
+//   IonText,
+// } from "@ionic/react";
+// // import { groupBy } from "lodash";
+// import Schedulecard from "../../components/Schedule-card/Schedulecard";
+// import HeaderButtons from "../../components/HeaderButtons";
+// import AlertError from "../../components/alerts/AlertError";
+// import AlertSuccess from "../../components/alerts/AlertSuccess";
+// import { calendar } from "ionicons/icons";
+// import LoadingSpinner from "../../components/loading-spinner/LoadingSpinner";
+// import { format } from "date-fns";
+// import ErrorComponent from "../../components/error-component/ErrorComponent";
+// import Toast from "../../components/Custom Toast/Toast";
+// // import DatePicker from "../../components/Date Picker/MyDatePicker";
+// interface IVaccine {
+//   Id: number;
+//   Name: string;
+//   MinAge: number;
+//   VaccineId: number;
+// }
+
+// interface IVaccineData {
+//   [date: string]: IVaccine[];
+// }
+
+// const ScheduleList1: React.FC = () => {
+//   const [data, setData] = useState<IVaccine[]>([]);
+//   const [groupedData, setGroupedData] = useState<IVaccineData[]>([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [success, setSuccess] = useState(false);
+//   const [error, setError] = useState(false);
+//   const [showPopover, setShowPopover] = useState(false);
+//   const [renderList, setRenderList] = useState(false);
+//   const [selectedDate, setSelectedDate] = useState<string | null>(null);
+//   const [inputValue, setInputValue] = useState("");
+//   const [value, setValue] = useState("");
+//   const [showLoading, setShowLoading] = useState(false);
+
+//   const forceRender = () => {
+//     fetchDoseData();
+//   };
+
+//   useEffect(() => {
+//     fetchDoseData();
+//   }, []);
+
+//   const fetchDoseData = async () => {
+//     try {
+//       const response = await fetch(
+//         `${
+//           import.meta.env.VITE_API_URL
+//         }api/AdminSchedule/admin_post_doseSchedule`
+//       );
+//       if (response.ok) {
+//         const data = await response.json();
+//         setShowLoading(true);
+//         setData(data);
+//         console.log(data);
+//         setIsLoading(false);
+//       } else {
+//         console.log("Error fetching data");
+//         setIsLoading(false);
+//       }
+//     } catch (error) {
+//       console.log("Error:", error);
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const handelonmouseover = (inputValue: string) => {
+//     // const data1 = inputValue.split("T");
+//     const data2 = format(new Date(inputValue), "yyyy-MM-dd");
+//     setValue(data2);
+//     setSelectedDate(data2);
+//   };
+
+//   const handleDateChange = async (
+//     event: CustomEvent,
+//     key: string,
+//     inputValue: string
+//   ) => {
+//     console.log(value);
+//     closePopover();
+//     const data = event.detail.value;
+//     // const data1 = data.split("T");
+//     // const data2 = data1[0];
+//     // console.log(data2);
+// console.log(data)
+//     console.log(event.detail.value);
+
+//     // const dataTobeSent = [
+//     //   {
+//     //     path: "Date",
+//     //     op: "replace",
+//     //     from: value,
+//     //     value: data2,
+//     //   },
+//     // ];
+
+//     // console.log("object item date : ", dataTobeSent);
+//     try {
+//       setShowLoading(true);
+//       const response = await fetch(
+//         `${
+//           import.meta.env.VITE_API_URL
+//         }api/AdminSchedule/admin_bulk_update_Date?oldDate=${value}&newDate=${data}`,
+//         {
+//           method: "PATCH",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           // body: JSON.stringify(dataTobeSent),
+//         }
+//       );
+//       console.log(
+//         `${
+//           import.meta.env.VITE_API_URL
+//         }api/AdminSchedule/admin_bulk_update_Date?oldDate=${value}&newDate=${data}`
+//       );
+//       if (response.status === 204) {
+//         setShowLoading(false);
+//         console.log(response.ok);
+//         setSuccess(true);
+//         forceRender();
+//       } else if (!response.ok) {
+//         setError(true);
+//         setShowLoading(false);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       setError(true);
+//       setShowLoading(false);
+//     }
+//   };
+
+//   const openPopover = () => {
+//     setShowPopover(true);
+//   };
+
+//   const closePopover = () => {
+//     setShowPopover(false);
+//   };
+
+//   return (
+//     <>
+//       <LoadingSpinner
+//         isOpen={showLoading}
+//         setOpen={setShowLoading}
+//         time={3000}
+//       />
+//       <Toast
+//         isOpen={success}
+//         setOpen={setSuccess}
+//         message="Bulk date of admin schedule update successfully."
+//         color="success"
+//       />
+//       <Toast
+//         isOpen={error}
+//         setOpen={setError}
+//         message="An error occurred while update bulk date of admin schedule. plz try again"
+//         color="danger"
+//       />
+//       {/* <AlertSuccess
+//         isOpen={success}
+//         setOpen={setSuccess}
+//         message="Selected dose date updated successfully"
+//       />
+//       <AlertError
+//         isOpen={error}
+//         setOpen={setError}
+//         message="An error occurred. Please try again."
+//       /> */}
+//       <IonPage>
+//         <HeaderButtons
+//           pageName="Dose Schedule"
+//           Showbutton={false}
+//           backbutton={true}
+//           url="/members/schedule"
+//         />
+//         <IonContent className="ion-padding">
+//           {Object.keys(data).map((date) => (
+//             <IonCard key={date}>
+//               <>
+//                 <IonItem lines="none" className="centered-item">
+//                   <IonLabel style={{ textAlign: "center" }}>
+//                     <IonItem
+//                       lines="none"
+//                       slot="center"
+//                       style={{ textAlign: "center", padding: 0 }}
+//                     >
+//                       <IonIcon
+//                         color="primary"
+//                         onClick={() => setShowPopover(true)}
+//                         icon={calendar}
+//                         style={{ marginRight: "10px", cursor: "pointer" }}
+//                         onMouseOver={() => handelonmouseover(date)}
+//                       />
+//                       <IonText>{format(new Date(date), "yyyy-MM-dd")}</IonText>
+//                       <IonPopover
+//                         isOpen={showPopover}
+//                         onDidDismiss={closePopover}
+//                       >
+//                         <IonDatetime
+//                           placeholder="Select Date"
+//                           value={selectedDate || undefined}
+//                           onIonChange={(e) =>
+//                             handleDateChange(e, date, inputValue)
+//                           }
+//                         ></IonDatetime>
+//                       </IonPopover>
+//                     </IonItem>
+//                   </IonLabel>
+//                 </IonItem>
+//                 {data && data[date] ? (
+//                   data[date].length > 0 ? (
+//                     data[date].map((item: IVaccine) =>
+//                       item !== null ? (
+//                         <Schedulecard
+//                           key={item.Id}
+//                           date={date}
+//                           Id={item.Id}
+//                           Name={item.Name}
+//                           MinAge={item.MinAge}
+//                           VaccineId={item.VaccineId}
+//                           renderList={forceRender}
+//                         />
+//                       ) : (
+//                         ""
+//                       )
+//                     )
+//                   ) : (
+//                     <ErrorComponent title="Admin Schedule" />
+//                   )
+//                 ) : (
+//                   <ErrorComponent title="Brands" />
+//                 )}
+//               </>
+//             </IonCard>
+//           ))}
+//         </IonContent>
+//       </IonPage>
+//     </>
+//   );
+// };
+
+// export default ScheduleList1;
