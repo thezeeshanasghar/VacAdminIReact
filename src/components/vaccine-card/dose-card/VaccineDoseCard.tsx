26f327bc2cc5d698a14571f6f5183f4b6e4b21b0
codewithumarrr <94783559+codewithumarrr@users.noreply.github.com>
2023-10-24 20:08:11 +0500

    bug fix

--- src/components/vaccine-card/dose-card/VaccineDoseCard.tsx
+++ src/components/vaccine-card/dose-card/VaccineDoseCard.tsx
@@ -1,82 +1,93 @@
 import {
-    IonButton,
-    IonCard,
-    IonCardContent,
-    IonIcon,
-    IonItem,
-    IonLabel,
-  } from "@ionic/react";
-  import { create, trash } from "ionicons/icons";
-  import React, { useState } from "react";
-  import "./vaccineDoseCard.css";
-  import { IDoseData } from "../../../pages/vaccines/doses/VaccineDoseCardList";
-  import DeletePopup from "../../delete-popup/DeletePopup";
-  interface IDoseDataWithMt extends IDoseData {
-    mt: boolean;
-    renderList: () => void;
-  }
-  const VaccineDoseCard: React.FC<IDoseDataWithMt> = ({
-    mt,
-    Id,
-    Name,
-    VaccineId,
-    MinAge,
-    // MinGap,
-    renderList,
-  }) => {
-    const [showPopup, setShowPopup] = useState(false);
-    return (
-      <>
-        <DeletePopup
-          url={`${import.meta.env.VITE_API_URL}api/Dose/${Id}`}
-          title="Dose"
-          confirmAlertOpen={showPopup}
-          setConfirmAlertOpen={setShowPopup}
-          renderList={renderList}
-        />
-        <IonCard style={{ marginTop: `${mt ? 0 : null}`, marginInline: 0 }}>
-          <IonItem>
-            <IonLabel>{Name}</IonLabel>
-            <IonItem slot="end">
-              <IonButton
-                fill="clear"
-                className="custom-ripple doses-buttons"
-                routerLink={`/members/vaccine/${VaccineId}/doses/edit/${Id}?doseName=${Name.replace('#', '-')}&minAge=${MinAge}`}
-                routerDirection="root"
-                id={`dosee${Id}`}
-              >
-                <IonIcon
-                  color="primary"
-                  icon={create}
-                  slot="end"
-                  tabIndex={0}
-                  aria-label="create"
-                />
-              </IonButton>
-              <IonButton
-                className="custom-ripple doses-buttons"
-                fill="clear"
-                expand="full"
-                onClick={() => setShowPopup(true)}
-                id={`dosed${Id}`}
-              >
-                <IonIcon
-                  color="primary"
-                  icon={trash}
-                  slot="end"
-                  aria-label="trash"
-                />
-              </IonButton>
-            </IonItem>
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+} from "@ionic/react";
+import { create, trash } from "ionicons/icons";
+import React, { useState } from "react";
+import "./vaccineDoseCard.css";
+import { IDoseData } from "../../../pages/vaccines/doses/VaccineDoseCardList";
+import DeletePopup from "../../delete-popup/DeletePopup";
+interface IDoseDataWithMt extends IDoseData {
+  mt: boolean;
+  renderList: () => void;
+}
+const VaccineDoseCard: React.FC<IDoseDataWithMt> = ({
+  mt,
+  Id,
+  Name,
+  VaccineId,
+  MinAge,
+  // MinGap,
+  renderList,
+}) => {
+  const [showPopup, setShowPopup] = useState(false);
+  return (
+    <>
+      <DeletePopup
+        url={`${import.meta.env.VITE_API_URL}api/Dose/${Id}`}
+        title="Dose"
+        confirmAlertOpen={showPopup}
+        setConfirmAlertOpen={setShowPopup}
+        renderList={renderList}
+      />
+      <IonCard style={{ marginTop: `${mt ? 0 : null}`, marginInline: 0 }}>
+        <IonItem>
+          <IonLabel>{Name}</IonLabel>
+          <IonItem slot="end">
+            <IonButton
+              fill="clear"
+              className="custom-ripple doses-buttons"
+              routerLink={`/members/vaccine/${VaccineId}/doses/edit/${Id}?doseName=${Name.replace(
+                "#",
+                "-"
+              )}&minAge=${MinAge}`}
+              routerDirection="root"
+              id={`dosee${Id}`}
+            >
+              <IonIcon
+                color="primary"
+                icon={create}
+                slot="end"
+                tabIndex={0}
+                aria-label="create"
+              />
+            </IonButton>
+            <IonButton
+              className="custom-ripple doses-buttons"
+              fill="clear"
+              expand="full"
+              onClick={() => setShowPopup(true)}
+              id={`dosed${Id}`}
+            >
+              <IonIcon
+                color="primary"
+                icon={trash}
+                slot="end"
+                aria-label="trash"
+              />
+            </IonButton>
           </IonItem>
-          <IonCardContent>
-            <p>Minimum Age Limit: {Math.floor(MinAge / 365) !== 0 ? `${Math.floor(MinAge / 365)} years` : ""} {Math.floor((MinAge%365)/30) !== 0 ? `${Math.floor((MinAge%365)/30)} Months` : ""} {Math.floor(((MinAge%365)%30)%7) !== 0 ? `${Math.floor(((MinAge%365)%30)%7)} Days` : ""}</p>
-            {/* <p>Minimum Gap Limit: {MinGap} Days</p> */}
-          </IonCardContent>
-        </IonCard>
-      </>
-    );
-  };
-  
-  export default VaccineDoseCard;
-  
+        </IonItem>
+        <IonCardContent>
+          <p>
+            Minimum Age Limit: &nbsp;
+            {MinAge === 0
+              ? "at birth"
+              : MinAge >= 365
+              ? `${Math.floor(MinAge / 365)} year`
+              : MinAge >= 30
+              ? `${Math.floor(MinAge / 30)} month`
+              : `${MinAge} day`}
+          </p>
+          {/* <p>Minimum Gap Limit: {MinGap} Days</p> */}
+        </IonCardContent>
+      </IonCard>
+    </>
+  );
+};
+
+export default VaccineDoseCard;
